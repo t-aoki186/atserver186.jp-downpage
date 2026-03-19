f4e325f79773d95fb68c7ef4df97e6a7317229a6
taoki <84th.tccaoki@gmail.com>
2026-03-20 08:33:20 +0900

    add: トップページに内容を追加

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/favicon.png';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="stylesheet" href="https://cdn.atserver186.jp/libs/fontawesome/css/all.min.css" />
+	<link rel="icon" href={favicon} />
+</svelte:head>
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,53 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<svelte:head>
+	<title>ATSERVER</title>
+</svelte:head>
+
+<main class="main-container container mx-auto">
+	<div class="flex h-screen flex-col justify-center">
+		<h1 class="mx-auto mb-6 text-2xl font-semibold md:mb-10 md:text-4xl">
+			ただいまメンテナンス中です
+		</h1>
+		<hr class="main-hr" />
+		<p class="mt-6 mb-6 text-lg md:mt-10 md:mb-10 md:text-xl">
+			現在、当サイトはサーバーメンテナンス中です。<br />復旧は2026年03月19日
+			23時00分を予定しております。<br
+			/>サーバーメンテナンスのためATSERVER関連のすべてのサービスがご利用いただけません。
+		</p>
+		<hr class="main-hr" />
+		<div class="mx-auto mt-6 hidden sm:flex md:mt-10">
+			<a href="/target" class="link-main mr-4">
+				<div class="link-main-underline">
+					<i class="fa-solid fa-arrow-right-long"></i>
+					<span>対象サービス</span>
+				</div>
+			</a>
+			<a href="/reason" class="link-main mr-4">
+				<div class="link-main-underline">
+					<i class="fa-solid fa-arrow-right-long"></i>
+					<span>サーバーメンテナンスの理由</span>
+				</div>
+			</a>
+			<a href="/contact" class="link-main">
+				<div class="link-main-underline">
+					<i class="fa-solid fa-envelope"></i>
+					<span>お問い合わせ</span>
+				</div>
+			</a>
+		</div>
+		<div class="mt-6 flex flex-col sm:hidden">
+			<a href="/target" class="mb-2 rounded-xl bg-white p-2">
+				<i class="fa-solid fa-arrow-right-long"></i>
+				<span>対象サービス</span>
+			</a>
+			<a href="/reason" class="mb-2 rounded-xl bg-white p-2">
+				<i class="fa-solid fa-arrow-right-long"></i>
+				<span>サーバーメンテナンスの理由</span>
+			</a>
+			<a href="/contact" class="mb-2 rounded-xl bg-white p-2">
+				<i class="fa-solid fa-envelope"></i>
+				<span>お問い合わせ</span>
+			</a>
+		</div>
+		<p class="mt-6 text-sm md:mt-10">&copy; 2026 ATSERVER | atserver186.jp All Rights Reserved.</p>
+	</div>
+</main>
