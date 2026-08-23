7ceddebb5001770f178288d85fd3421d7ea5a3ce
taoki <84th.tccaoki@gmail.com>
2026-08-23 23:27:57 +0900

    feat: 動的ビルドから静的ビルドに変更

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,10 @@
-// svelte.config.js
-import adapter from '@sveltejs/adapter-node'; // ここを変更
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    // adapter-node を使用
     adapter: adapter()
   }
 };
